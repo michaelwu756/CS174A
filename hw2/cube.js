b0d74bbc8e74cbb9e51d2f054ae7a23844631139
Michael Wu <cheeserules43@gmail.com>
2019-04-15 23:48:31 -0700

    finished hw2

--- hw2/cube.js
+++ hw2/cube.js
@@ -1,9 +1,9 @@
-
 var canvas;
 var gl;
 
-var NumVertices  = 36;
-                                   // For each face of the cube, make two triangles of three vertices each:
+var NumVertices  = 18;
+
+// For each face of the cube, make two triangles of three vertices each:
 var vertexPositions = [
     vec4( -0.5, 0.5, 0.5, 1 ),
     vec4( -0.5, -0.5, 0.5, 1 ),
@@ -12,43 +12,96 @@ var vertexPositions = [
     vec4( 0.5, -0.5, 0.5, 1 ),
     vec4( 0.5, 0.5, 0.5, 1 ),
 
-    vec4( 0.5, 0.5, 0.5, 1 ),
-    vec4( 0.5, -0.5, 0.5, 1 ),
-    vec4( 0.5, -0.5, -0.5, 1 ),
-    vec4( 0.5, 0.5, 0.5, 1 ),
-    vec4( 0.5, -0.5, -0.5, 1 ),    
-    vec4( 0.5, 0.5, -0.5, 1 ),
-
-    vec4( 0.5, -0.5, 0.5, 1 ),
-    vec4( -0.5, -0.5, 0.5, 1 ),
-    vec4( -0.5, -0.5, -0.5, 1 ),
-    vec4( 0.5, -0.5, 0.5, 1 ),
-    vec4( -0.5, -0.5, -0.5, 1 ),
-    vec4( 0.5, -0.5, -0.5, 1 ),
-
-    vec4( 0.5, 0.5, -0.5, 1 ),
-    vec4( -0.5, 0.5, -0.5, 1 ),
-    vec4( -0.5, 0.5, 0.5, 1 ),
-    vec4( 0.5, 0.5, -0.5, 1 ),
     vec4( -0.5, 0.5, 0.5, 1 ),
     vec4( 0.5, 0.5, 0.5, 1 ),
+    vec4( 0, 0, 0, 1 ),
 
-    vec4( -0.5, -0.5, -0.5, 1 ),
-    vec4( -0.5, 0.5, -0.5, 1 ),
-    vec4( 0.5, 0.5, -0.5, 1 ),
-    vec4( -0.5, -0.5, -0.5, 1 ),
-    vec4( 0.5, 0.5, -0.5, 1 ),
-    vec4( 0.5, -0.5, -0.5, 1 ),
-    
-    vec4( -0.5, 0.5, -0.5, 1 ),
-    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( 0.5, 0.5, 0.5, 1 ),
+    vec4( 0.5, -0.5, 0.5, 1 ),
+    vec4( 0, 0, 0, 1 ),
+
+    vec4( 0.5, -0.5, 0.5, 1 ),
     vec4( -0.5, -0.5, 0.5, 1 ),
-    vec4( -0.5, 0.5, -0.5, 1 ),
+    vec4( 0, 0, 0, 1 ),
+
     vec4( -0.5, -0.5, 0.5, 1 ),
-    vec4( -0.5, 0.5, 0.5, 1 )
+    vec4( -0.5, 0.5, 0.5, 1 ),
+    vec4( 0, 0, 0, 1 )
 ];
 
 var vertexColors = [
+    [ -0.5, 0.5, 0.5, 1 ],
+    [ -0.5, -0.5, 0.5, 1 ],
+    [ 0.5, -0.5, 0.5, 1 ],
+    [ -0.5, 0.5, 0.5, 1 ],
+    [ 0.5, -0.5, 0.5, 1 ],
+    [ 0.5, 0.5, 0.5, 1 ],
+
+    [ -0.5, 0.5, 0.5, 1 ],
+    [ 0, 1, 0, 1 ],
+    [ 0, 1, 0, 1 ],
+
+    [ 1, 0, 0, 1 ],
+    [ 0.5, -0.5, 0.5, 1 ],
+    [ 1, 0, 0, 1 ],
+
+    [ 0.5, -0.5, 0.5, 1 ],
+    [ -0.5, -0.5, 0.5, 1 ],
+    [ 0, 0, 0, 1 ],
+
+    [ -0.5, -0.5, 0.5, 1 ],
+    [ -0.5, 0.5, 0.5, 1 ],
+    [ 0, 0, 0, 1 ]
+];
+
+
+var NumVertices2 = 36;
+
+var vertexPositions2 = [
+    vec4( -0.5, 0, 0, 1 ),
+    vec4( -0.5, -0.5, 0, 1 ),
+    vec4( 0, -0.5, 0, 1 ),
+    vec4( -0.5, 0, 0, 1 ),
+    vec4( 0, -0.5, 0, 1 ),
+    vec4( 0, 0, 0, 1 ),
+
+    vec4( 0, 0, 0, 1 ),
+    vec4( 0, -0.5, 0, 1 ),
+    vec4( 0, -0.5, -0.5, 1 ),
+    vec4( 0, 0, 0, 1 ),
+    vec4( 0, -0.5, -0.5, 1 ),
+    vec4( 0, 0, -0.5, 1 ),
+
+    vec4( 0, -0.5, 0, 1 ),
+    vec4( -0.5, -0.5, 0, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( 0, -0.5, 0, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( 0, -0.5, -0.5, 1 ),
+
+    vec4( 0, 0, -0.5, 1 ),
+    vec4( -0.5, 0, -0.5, 1 ),
+    vec4( -0.5, 0, 0, 1 ),
+    vec4( 0, 0, -0.5, 1 ),
+    vec4( -0.5, 0, 0, 1 ),
+    vec4( 0, 0, 0, 1 ),
+
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( -0.5, 0, -0.5, 1 ),
+    vec4( 0, -0.5, -0.5, 1 ),
+    vec4( -0.5, 0, -0.5, 1 ),
+    vec4( 0, 0, -0.5, 1 ),
+    vec4( 0, -0.5, -0.5, 1 ),
+
+    vec4( -0.5, 0, -0.5, 1 ),
+    vec4( -0.5, -0.5, -0.5, 1 ),
+    vec4( -0.5, -0.5, 0, 1 ),
+    vec4( -0.5, 0, -0.5, 1 ),
+    vec4( -0.5, -0.5, 0, 1 ),
+    vec4( -0.5, 0, 0, 1 )
+];
+
+var vertexColors2 = [
     [ 1.0, 0.0, 0.0, 1.0 ], // red
     [ 1.0, 0.0, 0.0, 1.0 ],
     [ 1.0, 0.0, 0.0, 1.0 ],
@@ -100,46 +153,50 @@ var axis = 0;
 var theta = [ 0, 0, 0 ];
 
 var thetaLoc;
+var cBuffer;
+var cBuffer2;
+var vBuffer;
+var vBuffer2;
+var program;
 
 window.onload = function init()
 {
     canvas = document.getElementById( "gl-canvas" );
-    
+
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
-    
+
     gl.enable(gl.DEPTH_TEST);
 
     //
     //  Load shaders and initialize attribute buffers
     //
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
-    
-    var cBuffer = gl.createBuffer();
+
+    cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW );
 
-    var vColor = gl.getAttribLocation( program, "vColor" );
-    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vColor );
-
-    var vBuffer = gl.createBuffer();
+    vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexPositions), gl.STATIC_DRAW );
-    
 
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+    cBuffer2 = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer2 );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexColors2), gl.STATIC_DRAW );
 
-    thetaLoc = gl.getUniformLocation(program, "theta"); 
-    
+    vBuffer2 = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer2 );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexPositions2), gl.STATIC_DRAW );
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+
     //event listeners for buttons
-    
+
     document.getElementById( "xButton" ).onclick = function () {
         axis = xAxis;
     };
@@ -149,7 +206,7 @@ window.onload = function init()
     document.getElementById( "zButton" ).onclick = function () {
         axis = zAxis;
     };
-        
+
     render();
 }
 
@@ -160,7 +217,29 @@ function render()
     theta[axis] += 2.0;
     gl.uniform3fv(thetaLoc, theta);
 
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
     gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer2 );
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(vColor);
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer2 );
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices2 );
 
     requestAnimFrame( render );
 }
