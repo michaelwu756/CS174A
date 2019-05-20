f59797880bc2873cd1893fdafb0b1b62b2a8472e
Michael Wu <cheeserules43@gmail.com>
2019-05-19 18:41:12 -0700

    finished ec2

--- hw4/main-scene.js
+++ hw4/main-scene.js
@@ -21,14 +21,13 @@ const Main_Scene =
       this.shapes.ball_5.arrays.texture_coord.forEach(coord => coord.scale(5));
 
       const phong_shader = new defs.Phong_Shader(2);
-      const texture_shader = new defs.Textured_Phong(2);
       const texture_shader_2 = new defs.Fake_Bump_Map(2);
       const gouraud_shader = new Gouraud_Shader(2);
       const black_hole_shader = new Black_Hole_Shader();
       const sun_shader = new Sun_Shader();
 
       this.materials = {
-        sun: new Material(phong_shader, { ambient: 1, diffusivity: 0, specularity: 0 }),
+        sun: new Material(sun_shader),
         planet_1: new Material(phong_shader, { diffusivity: 0.9, specularity: 0.1, color: Color.of(0.5, 0.5, 0.5, 1) }),
         planet_2: new Material(phong_shader, { diffusivity: 0, specularity: 1, color: Color.of(0.41, 0.41, 0.41, 1) }),
         planet_3: new Material(texture_shader_2, { texture: new Texture("assets/earth.gif"), diffusivity: 0.9, specularity: 0.1, color: Color.of(0, 0.5, 0.5, 1) }),
@@ -64,7 +63,7 @@ const Main_Scene =
       this.camera_teleporter.cameras.push(Mat4.look_at(Vec.of(0, 10, 20), Vec.of(0, 0, 0), Vec.of(0, 1, 0)));
 
       let model_transform = Mat4.identity();
-      const modifier = this.lights_on ? { ambient: 0.3 } : { ambient: 0.0 };
+      const modifier = this.lights_on ? { ambient: 0.3 } : { ambient: 0. };
 
       const smoothly_varying_ratio = .5 + .5 * Math.sin(2 * Math.PI * t / 10),
         sun_size = 1 + 2 * smoothly_varying_ratio,
@@ -129,7 +128,6 @@ const Additional_Scenes = [];
 
 export { Main_Scene, Additional_Scenes, Canvas_Widget, Code_Widget, Text_Widget, defs }
 
-
 const Camera_Teleporter = defs.Camera_Teleporter =
   class Camera_Teleporter extends Scene {
     // **Camera_Teleporter** is a helper Scene meant to be added as a child to
@@ -164,7 +162,6 @@ const Camera_Teleporter = defs.Camera_Teleporter =
       program_state.set_camera(desired_camera.map((x, i) => Vec.from(program_state.camera_inverse[i]).mix(x, .01 * dt)));
     }
   }
-
 
 const Planar_Star = defs.Planar_Star =
   class Planar_Star extends Shape {
@@ -205,27 +202,26 @@ const Gouraud_Shader = defs.Gouraud_Shader =
         varying vec4 color;
         vec3 gouraud_model_lights(vec3 N, vec3 vertex_worldspace) {
           vec3 E = normalize(camera_center - vertex_worldspace);
-          vec3 result = vec3(0.0);
+          vec3 result = vec3(0.);
           for (int i = 0; i < N_LIGHTS; i++)
           {
             vec3 surface_to_light_vector = light_positions_or_vectors[i].xyz -
               light_positions_or_vectors[i].w * vertex_worldspace;
             float distance_to_light = length(surface_to_light_vector);
-  
+
             vec3 L = normalize(surface_to_light_vector);
             vec3 H = normalize(L + E);
-            float diffuse = max(dot(N, L), 0.0);
-            float specular = pow(max(dot(N, H), 0.0), smoothness);
-            float attenuation = 1.0 / (1.0 + light_attenuation_factors[i] * distance_to_light * distance_to_light);
-  
-            vec3 light_contribution = shape_color.xyz * vec3(1.0, 1.0, 1.0) * diffusivity * diffuse
-              + vec3(1.0, 1.0, 1.0) * specularity * specular;
-  
+            float diffuse = max(dot(N, L), 0.);
+            float specular = pow(max(dot(N, H), 0.), smoothness);
+            float attenuation = 1. / (1. + light_attenuation_factors[i] * distance_to_light * distance_to_light);
+
+            vec3 light_contribution = shape_color.xyz * vec3(1., 1., 1.) * diffusivity * diffuse
+              + vec3(1., 1., 1.) * specularity * specular;
+
             result += attenuation * light_contribution;
           }
           return result;
         }`;
-
     }
     vertex_glsl_code() {
       return this.shared_glsl_code() + `
@@ -233,9 +229,9 @@ const Gouraud_Shader = defs.Gouraud_Shader =
         uniform mat4 model_transform;
         uniform mat4 projection_camera_model_transform;
         void main() {
-          gl_Position = projection_camera_model_transform * vec4(position, 1.0);
+          gl_Position = projection_camera_model_transform * vec4(position, 1.);
           vec3 N = normalize(mat3(model_transform) * normal / squared_scale);
-          vec3 vertex_worldspace = (model_transform * vec4(position, 1.0)).xyz;
+          vec3 vertex_worldspace = (model_transform * vec4(position, 1.)).xyz;
           color = vec4(shape_color.xyz * ambient, shape_color.w);
           color.xyz += gouraud_model_lights(normalize(N), vertex_worldspace);
         }`;
@@ -274,7 +270,6 @@ const Black_Hole_Shader = defs.Black_Hole_Shader =
           gl_Position = projection_camera_model_transform * vec4(newPosition, 1.);
           f_tex_coord = texture_coord;
         }`;
-
     }
     fragment_glsl_code() {
       return this.shared_glsl_code() + `
@@ -287,39 +282,139 @@ const Black_Hole_Shader = defs.Black_Hole_Shader =
     }
   }
 
-
 const Sun_Shader = defs.Sun_Shader =
   class Sun_Shader extends Shader {
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-      // TODO (#EC 2): Pass the same information to the shader as for EC part 1.  Additionally
-      // pass material.color to the shader.
-
-
+    update_GPU(context, gpu_addresses, program_state, model_transform, material) {
+      const [P, C, M] = [program_state.projection_transform, program_state.camera_inverse, model_transform],
+        PCM = P.times(C).times(M);
+      context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false, Mat.flatten_2D_to_1D(PCM.transposed()));
+      context.uniform1f(gpu_addresses.time, program_state.animation_time / 1000);
+      context.uniform4fv(gpu_addresses.sun_color, material.color);
     }
-    // TODO (#EC 2):  Complete the shaders, displacing the input sphere's vertices as
-    // a fireball effect and coloring fragments according to displacement.
-
-    shared_glsl_code()            // ********* SHARED CODE, INCLUDED IN BOTH SHADERS *********
-    {
-      return `precision mediump float;
-
-      `;
+    shared_glsl_code() {
+      return `
+        precision mediump float;
+        varying float disp;`;
     }
-    vertex_glsl_code()           // ********* VERTEX SHADER *********
-    {
+    vertex_glsl_code() {
       return this.shared_glsl_code() + `
+        attribute vec3 position;
+        attribute vec3 normal;
+        attribute vec2 uv;
+        attribute vec2 uv2;
+        uniform mat4 projection_camera_model_transform;
+        uniform float time;
+        vec3 mod289(vec3 x) {
+          return x - floor(x * (1. / 289.)) * 289.;
+        }
+        vec4 mod289(vec4 x) {
+          return x - floor(x * (1. / 289.)) * 289.;
+        }
+        vec4 permute(vec4 x) {
+          return mod289(((x * 34.) + 1.) * x);
+        }
+        vec4 taylorInvSqrt(vec4 r) {
+          return 1.79284291400159 - 0.85373472095314 * r;
+        }
+        vec3 fade(vec3 t) {
+          return t * t * t * (t * (t * 6. - 15.) + 10.);
+        }
+        float cnoise(vec3 P) {
+          vec3 Pi0 = floor(P);
+          vec3 Pi1 = Pi0 + vec3(1.);
+          Pi0 = mod289(Pi0);
+          Pi1 = mod289(Pi1);
+          vec3 Pf0 = fract(P);
+          vec3 Pf1 = Pf0 - vec3(1.);
+          vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+          vec4 iy = vec4(Pi0.yy, Pi1.yy);
+          vec4 iz0 = Pi0.zzzz;
+          vec4 iz1 = Pi1.zzzz;
 
-        void main()
-        {
+          vec4 ixy = permute(permute(ix) + iy);
+          vec4 ixy0 = permute(ixy + iz0);
+          vec4 ixy1 = permute(ixy + iz1);
 
+          vec4 gx0 = ixy0 * (1. / 7.);
+          vec4 gy0 = fract(floor(gx0) * (1. / 7.)) - 0.5;
+          gx0 = fract(gx0);
+          vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+          vec4 sz0 = step(gz0, vec4(0.));
+          gx0 -= sz0 * (step(0., gx0) - 0.5);
+          gy0 -= sz0 * (step(0., gy0) - 0.5);
+
+          vec4 gx1 = ixy1 * (1. / 7.);
+          vec4 gy1 = fract(floor(gx1) * (1. / 7.)) - 0.5;
+          gx1 = fract(gx1);
+          vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+          vec4 sz1 = step(gz1, vec4(0.));
+          gx1 -= sz1 * (step(0., gx1) - 0.5);
+          gy1 -= sz1 * (step(0., gy1) - 0.5);
+
+          vec3 g000 = vec3(gx0.x, gy0.x, gz0.x);
+          vec3 g100 = vec3(gx0.y, gy0.y, gz0.y);
+          vec3 g010 = vec3(gx0.z, gy0.z, gz0.z);
+          vec3 g110 = vec3(gx0.w, gy0.w, gz0.w);
+          vec3 g001 = vec3(gx1.x, gy1.x, gz1.x);
+          vec3 g101 = vec3(gx1.y, gy1.y, gz1.y);
+          vec3 g011 = vec3(gx1.z, gy1.z, gz1.z);
+          vec3 g111 = vec3(gx1.w, gy1.w, gz1.w);
+
+          vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+          g000 *= norm0.x;
+          g010 *= norm0.y;
+          g100 *= norm0.z;
+          g110 *= norm0.w;
+          vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+          g001 *= norm1.x;
+          g011 *= norm1.y;
+          g101 *= norm1.z;
+          g111 *= norm1.w;
+
+          float n000 = dot(g000, Pf0);
+          float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+          float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+          float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+          float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+          float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+          float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+          float n111 = dot(g111, Pf1);
+
+          vec3 fade_xyz = fade(Pf0);
+          vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+          vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+          float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x);
+          return 2.2 * n_xyz;
+        }
+        float turbulence(vec3 p) {
+          float t = -0.5;
+          for (float f = 1.; f <= 10. ; f++ ) {
+            float power = pow(2., f);
+            t += abs(cnoise(vec3(power * p)) / power);
+          }
+          return t;
+        }
+
+        void main() {
+          float pulseHeight = 0.2;
+          float displacementHeight = 0.65269959;
+          float turbulenceDetail = 0.63107728;
+          float noise = -0.8 * turbulence(turbulenceDetail * normal + (time * 0.2));
+          float displacement = (0. - displacementHeight) * noise + pulseHeight * cnoise(0.05 * position + vec3(0.2 * time));
+          disp = displacement;
+
+          vec3 newPosition = position + normal * displacement;
+          gl_Position = projection_camera_model_transform * vec4(newPosition, 1.);
         }`;
     }
-    fragment_glsl_code()           // ********* FRAGMENT SHADER *********
-    {
+    fragment_glsl_code() {
       return this.shared_glsl_code() + `
-        void main()
-        {
+        uniform vec4 sun_color;
 
-        } ` ;
+        void main() {
+            vec3 color = vec3((1. - 15. * disp), (0.1 - disp * 3.) + 0.3, (0.1 - disp * 1.5) + 0.1 * abs(sin(10. * disp)));
+            gl_FragColor = vec4(color.rgb, 1.);
+            gl_FragColor *= sun_color;
+        }`;
     }
   }
